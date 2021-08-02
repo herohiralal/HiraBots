4b039845bae725a5d120d4707644c9e56de7960e
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-08-03 00:01:23 +0530

    Changed ReadOnlyAccessors access modifier to make it available to PublicAPI.

--- Scripts/Runtime/Utils/ReadOnlyAccessors.cs
+++ Scripts/Runtime/Utils/ReadOnlyAccessors.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 
 namespace HiraBots
 {
-    internal readonly struct ReadOnlyArrayAccessor<T> : IEnumerable<T>
+    public readonly struct ReadOnlyArrayAccessor<T> : IEnumerable<T>
     {
         private ReadOnlyArrayAccessor(T[] collection) => _collection = collection;
         private readonly T[] _collection;
-        internal int Count => _collection.Length;
-        internal T this[int index] => _collection[index];
+        public int Count => _collection.Length;
+        public T this[int index] => _collection[index];
         public Enumerator GetEnumerator() => new Enumerator(_collection);
         IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>) _collection).GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _collection).GetEnumerator();
         public static implicit operator ReadOnlyArrayAccessor<T>(T[] i) => new ReadOnlyArrayAccessor<T>(i);
 
-        internal struct Enumerator : IEnumerator<T>
+        public struct Enumerator : IEnumerator<T>
         {
-            internal Enumerator(T[] collection)
+            public Enumerator(T[] collection)
             {
                 _collection = collection;
                 _currentIndex = -1;
@@ -46,28 +46,28 @@ namespace HiraBots
         }
     }
 
-    internal readonly struct ReadOnlyListAccessor<T> : IEnumerable<T>
+    public readonly struct ReadOnlyListAccessor<T> : IEnumerable<T>
     {
         private ReadOnlyListAccessor(List<T> collection) => _collection = collection;
         private readonly List<T> _collection;
-        internal int Count => _collection.Count;
-        internal T this[int index] => _collection[index];
+        public int Count => _collection.Count;
+        public T this[int index] => _collection[index];
         public List<T>.Enumerator GetEnumerator() => _collection.GetEnumerator();
         IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>) _collection).GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _collection).GetEnumerator();
         public static implicit operator ReadOnlyListAccessor<T>(List<T> i) => new ReadOnlyListAccessor<T>(i);
     }
 
-    internal readonly struct ReadOnlyDictionaryAccessor<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    public readonly struct ReadOnlyDictionaryAccessor<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
     {
         private ReadOnlyDictionaryAccessor(Dictionary<TKey, TValue> collection) => _collection = collection;
         private readonly Dictionary<TKey, TValue> _collection;
-        internal int Count => _collection.Count;
-        internal bool ContainsKey(TKey key) => _collection.ContainsKey(key);
-        internal TValue this[TKey key] => _collection[key];
-        internal bool TryGetValue(TKey key, out TValue value) => _collection.TryGetValue(key, out value);
-        internal Dictionary<TKey, TValue>.KeyCollection Keys => _collection.Keys;
-        internal Dictionary<TKey, TValue>.ValueCollection Values => _collection.Values;
+        public int Count => _collection.Count;
+        public bool ContainsKey(TKey key) => _collection.ContainsKey(key);
+        public TValue this[TKey key] => _collection[key];
+        public bool TryGetValue(TKey key, out TValue value) => _collection.TryGetValue(key, out value);
+        public Dictionary<TKey, TValue>.KeyCollection Keys => _collection.Keys;
+        public Dictionary<TKey, TValue>.ValueCollection Values => _collection.Values;
         public Dictionary<TKey, TValue>.Enumerator GetEnumerator() => _collection.GetEnumerator();
         IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => ((IEnumerable<KeyValuePair<TKey, TValue>>) _collection).GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _collection).GetEnumerator();
