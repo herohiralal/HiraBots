a6e81169cb8402a8f9e4afaedd8a9311167ea515
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-08-24 00:28:16 +0530

    Added blackboard registering to code-gen.

--- Scripts/Runtime/Export/CodeGen/Base_GeneratedBlackboardComponent.cs
+++ Scripts/Runtime/Export/CodeGen/Base_GeneratedBlackboardComponent.cs
@@ -1,8 +1,24 @@
-﻿namespace UnityEngine
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine
 {
     // ReSharper disable once InconsistentNaming
-    public abstract class Base_GeneratedBlackboardComponent
+    public abstract class Base_GeneratedBlackboardComponent : IDisposable
     {
-        
+        private static readonly HashSet<Base_GeneratedBlackboardComponent> s_ActiveComponents =
+            new HashSet<Base_GeneratedBlackboardComponent>();
+
+        protected static void Register(Base_GeneratedBlackboardComponent component)
+        {
+            s_ActiveComponents.Add(component);
+        }
+
+        protected static void Unregister(Base_GeneratedBlackboardComponent component)
+        {
+            s_ActiveComponents.Remove(component);
+        }
+
+        public abstract void Dispose();
     }
 }
