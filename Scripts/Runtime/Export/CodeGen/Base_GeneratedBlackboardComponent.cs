7ae03cc990ea9a72289224a07b5af8b973da9065
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-08-24 02:44:08 +0530

    Added unexpected change reporting to codegen blackboard.

--- Scripts/Runtime/Export/CodeGen/Base_GeneratedBlackboardComponent.cs
+++ Scripts/Runtime/Export/CodeGen/Base_GeneratedBlackboardComponent.cs
@@ -9,6 +9,17 @@ namespace UnityEngine
         private static readonly HashSet<Base_GeneratedBlackboardComponent> s_ActiveComponents =
             new HashSet<Base_GeneratedBlackboardComponent>();
 
+        protected readonly List<string> m_UnexpectedChanges = new List<string>();
+
+        public ReadOnlyListAccessor<string> unexpectedChanges => m_UnexpectedChanges;
+
+        public bool hasUnexpectedChanges => m_UnexpectedChanges.Count > 0;
+
+        public void ClearUnexpectedChanges()
+        {
+            m_UnexpectedChanges.Clear();
+        }
+
         protected static void Register(Base_GeneratedBlackboardComponent component)
         {
             s_ActiveComponents.Add(component);
