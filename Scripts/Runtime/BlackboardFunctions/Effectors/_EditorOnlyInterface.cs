9e2617f0f4cdc21c402e1462abfbf6f67bab2e4f
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-11-23 10:19:08 +0530

    Fixed incorrect validation in effectors.

--- Scripts/Runtime/BlackboardFunctions/Effectors/_EditorOnlyInterface.cs
+++ Scripts/Runtime/BlackboardFunctions/Effectors/_EditorOnlyInterface.cs
@@ -9,17 +9,17 @@ namespace HiraBots
         {
             base.OnValidate();
 
+            if (m_OperationType == OperationType.Divide && m_Value == 0f)
+            {
+                m_Value = 0.1f;
+            }
+
             m_Key.keyTypesFilter = BlackboardKeyType.Float;
         }
 
         internal override void OnTargetBlackboardTemplateChanged(BlackboardTemplate newTemplate, ReadOnlyHashSetAccessor<BlackboardKey> keySet)
         {
             base.OnTargetBlackboardTemplateChanged(newTemplate, keySet);
-
-            if (m_OperationType == OperationType.Divide && m_Value == 0f)
-            {
-                m_Value = 0.1f;
-            }
 
             m_Key.OnTargetBlackboardTemplateChanged(newTemplate, keySet);
         }
@@ -31,17 +31,17 @@ namespace HiraBots
         {
             base.OnValidate();
 
+            if (m_OperationType == OperationType.Divide && m_Value == 0)
+            {
+                m_Value = 1;
+            }
+
             m_Key.keyTypesFilter = BlackboardKeyType.Integer;
         }
 
         internal override void OnTargetBlackboardTemplateChanged(BlackboardTemplate newTemplate, ReadOnlyHashSetAccessor<BlackboardKey> keySet)
         {
             base.OnTargetBlackboardTemplateChanged(newTemplate, keySet);
-
-            if (m_OperationType == OperationType.Divide && m_Value == 0)
-            {
-                m_Value = 1;
-            }
 
             m_Key.OnTargetBlackboardTemplateChanged(newTemplate, keySet);
         }
