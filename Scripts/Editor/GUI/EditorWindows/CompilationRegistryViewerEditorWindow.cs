5382bbfc66b04f3233bf3ca6bf4b3e36d4093b3a
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-12-17 08:18:39 +0530

    Added info viewing in CompilationRegistryViewer.

--- Scripts/Editor/GUI/EditorWindows/CompilationRegistryViewerEditorWindow.cs
+++ Scripts/Editor/GUI/EditorWindows/CompilationRegistryViewerEditorWindow.cs
@@ -14,8 +14,11 @@ namespace HiraBots.Editor
 
         private const string k_SelectObject = "Select Object";
 
-        [SerializeField] private Vector2 m_Scroller;
+        [SerializeField] private Vector2 m_ContentScroller;
+        [SerializeField] private Vector2 m_InfoScroller;
         [NonSerialized] private string m_CurrentObject = k_SelectObject;
+        [NonSerialized] private int m_CurrentDepth = 0;
+        [NonSerialized] private int m_CurrentIndex = 0;
 
         private void OnGUI()
         {
@@ -38,7 +41,7 @@ namespace HiraBots.Editor
 
             if (CompilationRegistry.database.TryGetValue(m_CurrentObject, out var dataForCurrentObject))
             {
-                var contentRect = new Rect(0, buttonRect.y + buttonRect.height, position.width * 0.66f, position.height - buttonRect.height);
+                var contentRect = new Rect(0, buttonRect.y + buttonRect.height + 10, position.width * 0.66f, position.height - buttonRect.height - 10);
 
                 var startAddress = dataForCurrentObject[0][0].startAddress;
                 var width = dataForCurrentObject[0][0].size * 20;
@@ -49,7 +52,7 @@ namespace HiraBots.Editor
 
                 var viewRect = new Rect(0, 0, width, dataForCurrentObject.count * 20);
 
-                m_Scroller = GUI.BeginScrollView(contentRect, m_Scroller, viewRect);
+                m_ContentScroller = GUI.BeginScrollView(contentRect, m_ContentScroller, viewRect);
 
                 for (var currentDepth = 0; currentDepth < dataForCurrentObject.count; currentDepth++)
                 {
@@ -62,8 +65,44 @@ namespace HiraBots.Editor
                         r.x = ((int) ((long) entry.startAddress - (long) startAddress) * 20) + 1;
                         r.width = (entry.size * 20) - 2;
 
-                        EditorGUI.DrawRect(r, Color.black);
-                        EditorGUI.LabelField(r, entry.name);
+                        if (GUI.Button(r, entry.name))
+                        {
+                            m_CurrentDepth = currentDepth;
+                            m_CurrentIndex = i;
+                        }
+                    }
+                }
+
+                GUI.EndScrollView();
+
+                var infoRect = new Rect(contentRect.x + contentRect.width + 10, contentRect.y, position.width - contentRect.width - 10, contentRect.height);
+
+                viewRect = new Rect(0, 0, 300, 21 * 4);
+
+                m_InfoScroller = GUI.BeginScrollView(infoRect, m_InfoScroller, viewRect);
+
+                r.x = 0;
+                r.y = 0;
+                r.width = 300;
+                r.height = 21;
+
+                if (m_CurrentDepth < dataForCurrentObject.count)
+                {
+                    if (m_CurrentIndex < dataForCurrentObject[m_CurrentDepth].count)
+                    {
+                        var entry = dataForCurrentObject[m_CurrentDepth][m_CurrentIndex];
+
+                        EditorGUI.LabelField(r, "Label", entry.name);
+                        r.y += r.height;
+
+                        EditorGUI.LabelField(r, "Start Address", entry.startAddress.ToString("X8"));
+                        r.y += r.height;
+
+                        EditorGUI.LabelField(r, "Last Address", entry.endAddress.ToString("X8"));
+                        r.y += r.height;
+
+                        EditorGUI.LabelField(r, "Size (in bytes)", entry.size.ToString());
+                        r.y += r.height;
                     }
                 }
 
