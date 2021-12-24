fb5e6f9c02b46d3f5ce8108145e43f190c24f1a7
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-12-24 11:54:51 +0530

    Created executable interfaces.

---
+++ Scripts/Runtime/Export/Executables/HiraBotsServiceProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityEngine
+{
+    public interface IHiraBotsService
+    {
+        void Start();
+        void Tick();
+        void Stop();
+    }
+
+    public abstract class HiraBotsServiceProvider : ScriptableObject
+    {
+        public abstract IHiraBotsService GetService(BlackboardComponent blackboard);
+    }
+}
---
+++ Scripts/Runtime/Export/Executables/HiraBotsTaskProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace UnityEngine
+{
+    public enum HiraBotsTaskResult : byte
+    {
+        InProgress,
+        Succeeded,
+        Failed
+    }
+
+    public interface IHiraBotsTask
+    {
+        void Begin();
+        HiraBotsTaskResult Execute(float deltaTime);
+        void Abort();
+        void End(bool success);
+    }
+
+    public abstract class HiraBotsTaskProvider : ScriptableObject
+    {
+        public abstract IHiraBotsTask GetTask(BlackboardComponent blackboard);
+    }
+}
