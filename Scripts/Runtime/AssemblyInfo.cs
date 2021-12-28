f46ac15b0019b2ea706f1fddd0817858e47f2611
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-12-28 07:37:50 +0530

    Removed: visible internals to generated code.

--- Scripts/Runtime/AssemblyInfo.cs
+++ Scripts/Runtime/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("HiraBots.Runtime")]
-[assembly: InternalsVisibleTo("HiraBots.CodeGen")]
 [assembly: InternalsVisibleTo("HiraBots.Editor")]
 [assembly: InternalsVisibleTo("HiraBots.Tests.EditMode")]
 [assembly: InternalsVisibleTo("HiraBots.Tests.PlayMode")]
