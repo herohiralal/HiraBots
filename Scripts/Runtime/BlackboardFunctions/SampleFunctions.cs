17defc9395d3a4788d8385264f641f2a671864e0
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-12-30 11:25:05 +0530

    Created the first sample function.

--- Scripts/Runtime/BlackboardFunctions/SampleFunctions.cs
+++ Scripts/Runtime/BlackboardFunctions/SampleFunctions.cs
@@ -12,7 +12,16 @@
 
     internal static class SampleDecoratorBlackboardFunctions
     {
+        [GenerateInternalBlackboardFunction("88371d2dafc44604aee17d7062a33f02")]
+        internal static bool AlwaysSucceedDecoratorBlackboardFunction(UnityEngine.BlackboardComponent blackboard, bool expected)
+        {
+            return true;
+        }
 
+        internal static bool AlwaysSucceedDecoratorBlackboardFunctionUnmanaged(UnityEngine.BlackboardComponent.LowLevel blackboard)
+        {
+            return true;
+        }
     }
 
     internal static class SampleScoreCalculatorBlackboardFunctions
