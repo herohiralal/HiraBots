6720c616f18875e7a9819cd58cc04cc4e8361c37
Rohan Jadav <ro1.darkstar7@gmail.com>
2021-12-30 19:39:27 +0530

    Aggressively inlined all sample blackboard functions.

--- Scripts/Runtime/BlackboardFunctions/SampleFunctions.cs
+++ Scripts/Runtime/BlackboardFunctions/SampleFunctions.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Mathematics;
+﻿using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace HiraBots
@@ -35,12 +36,14 @@ namespace HiraBots
 
     internal static class SampleDecoratorBlackboardFunctions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("88371d2dafc44604aee17d7062a33f02")]
         internal static bool AlwaysSucceedDecorator(bool invert)
         {
             return !invert;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("76cf91d3f677494e89e4545c520c6644")]
         internal static bool EnumEqualsDecorator(bool invert, ref byte key, [MatchTypeToEnumKey("key")] byte value)
         {
@@ -48,6 +51,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948b9a5ff847474ea57")]
         internal static bool EnumHasFlagsDecorator(bool invert, ref byte key, [MatchTypeToEnumKey("key")] byte value)
         {
@@ -55,6 +59,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948b9b5ff847474ea57")]
         internal static bool BooleanIsSetDecorator(bool invert, ref bool key)
         {
@@ -62,6 +67,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948c9a5ff847474ea57")]
         internal static bool QuaternionIsSetDecorator(bool invert, ref quaternion key)
         {
@@ -70,6 +76,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94d48b9a5ff847474ea57")]
         internal static bool VectorIsSetDecorator(bool invert, ref float3 key)
         {
@@ -78,6 +85,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("3b6d337fe0fa4b269f045171ef4871cd")]
         internal static bool IntegerComparisonDecorator(bool invert, ref int key, int secondValue, IntegerComparisonType comparisonType)
         {
@@ -107,6 +115,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("3b6d337fe0fa4b269f145171ef4871cd")]
         internal static bool FloatComparisonDecorator(bool invert, ref float key, float secondValue, float equalityTolerance, FloatComparisonType comparisonType)
         {
@@ -136,6 +145,7 @@ namespace HiraBots
             return invert != result;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("e1ecc27574ca4a030ff85de39d096ccb")]
         internal static bool ObjectEqualsDecorator(bool invert, [HiraBotsObjectKey] ref int key, [HiraBotsObjectValue] int value)
         {
@@ -146,54 +156,63 @@ namespace HiraBots
 
     internal static class SampleScoreCalculatorBlackboardFunctions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("88371d2dafc44604aee17d7062a33f0c")]
         internal static float AlwaysSucceedScoreCalculator(float currentScore, bool invert, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.AlwaysSucceedDecorator(invert) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("76cf91d3f677494e89e4545c520c664c")]
         internal static float EnumEqualsScoreCalculator(float currentScore, bool invert, ref byte key, [MatchTypeToEnumKey("key")] byte value, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.EnumEqualsDecorator(invert, ref key, value) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948b9a5ff847474ea5c")]
         internal static float EnumHasFlagsScoreCalculator(float currentScore, bool invert, ref byte key, [MatchTypeToEnumKey("key")] byte value, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.EnumHasFlagsDecorator(invert, ref key, value) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948b9b5ff847474ea5c")]
         internal static float BooleanIsSetScoreCalculator(float currentScore, bool invert, ref bool key, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.BooleanIsSetDecorator(invert, ref key) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94948c9a5ff847474ea5c")]
         internal static float QuaternionIsSetScoreCalculator(float currentScore, bool invert, ref quaternion key, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.QuaternionIsSetDecorator(invert, ref key) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("841fea4ba9b94d48b9a5ff847474ea5c")]
         internal static float VectorIsSetScoreCalculator(float currentScore, bool invert, ref float3 key, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.VectorIsSetDecorator(invert, ref key) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("3b6d337fe0fa4b269f045171ef4871cc")]
         internal static float IntegerComparisonScoreCalculator(float currentScore, bool invert, ref int key, int secondValue, IntegerComparisonType comparisonType, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.IntegerComparisonDecorator(invert, ref key, secondValue, comparisonType) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("3b6d337fe0fa4b269f145171ef4871cc")]
         internal static float FloatComparisonScoreCalculator(float currentScore, bool invert, ref float key, float secondValue, float equalityTolerance, FloatComparisonType comparisonType, float score)
         {
             return currentScore + (SampleDecoratorBlackboardFunctions.FloatComparisonDecorator(invert, ref key, secondValue, equalityTolerance, comparisonType) ? score : 0f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("e1ecc27574ca4a030ff85de39d096ccc")]
         internal static float ObjectEqualsScoreCalculator(float currentScore, bool invert, [HiraBotsObjectKey] ref int key, [HiraBotsObjectValue] int value, float score)
         {
@@ -241,6 +260,7 @@ namespace HiraBots
 
     internal static class SampleEffectorBlackboardFunctions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("ad6818e4de9846adba80edd01785671e")]
         internal static void EnumOperatorEffector(ref byte key, EnumOperationType operationType, [MatchTypeToEnumKey("key")] byte value)
         {
@@ -257,6 +277,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("89af0e100fac43f1ba83126b2d7c87de")]
         internal static void FloatOperatorEffector(ref float key, FloatOperationType operationType, float value)
         {
@@ -290,6 +311,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("f073722b6c814acdb3f6485ca22b0f0e")]
         internal static void IntegerOperatorEffector(ref int key, IntegerOperationType operationType, int value)
         {
@@ -324,6 +346,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("bf5cce8d3ca6458bbcf415de101c99e1")]
         internal static void BooleanIsSetEffector(ref bool key, SetOperationType operationType)
         {
@@ -340,6 +363,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("3e0f93fa04dd434da94e08d68526c5b0")]
         internal static void VectorIsSetEffector(ref float3 key, SetOperationType operationType)
         {
@@ -356,6 +380,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("81995b480d8141c2903d789771e5cd7c")]
         internal static void QuaternionIsSetEffector(ref quaternion key, SetOperationType operationType)
         {
@@ -372,6 +397,7 @@ namespace HiraBots
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         [GenerateInternalBlackboardFunction("da5818b29555439cb0a6adc4d0937280")]
         // ReSharper disable once RedundantAssignment
         internal static void ObjectEqualsEffector([HiraBotsObjectKey] ref int key, [HiraBotsObjectValue] int value)
