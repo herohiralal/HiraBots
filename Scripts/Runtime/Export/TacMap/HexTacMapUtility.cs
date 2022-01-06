5f3461637548ac3a110846017b1fb4f8faa5b1ed
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-01-07 00:47:39 +0530

    Removed unnecessary hexagonal coordinate types.

--- Scripts/Runtime/Export/TacMap/HexTacMapUtility.cs
+++ Scripts/Runtime/Export/TacMap/HexTacMapUtility.cs
@@ -6,52 +6,6 @@ namespace UnityEngine
     public static class HexTacMapUtility
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float OuterRadiusToInnerRadius(float f)
-        {
-            return f * 0.866025404f; // sqrt(3) / 2
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float InnerRadiusToOuterRadius(float f)
-        {
-            return f * 1.154700538f; // 2 / sqrt(3)
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float2 OffsetToPositionL(OffsetHexCoordinates o, float innerRadius)
-        {
-            var x = (o.column + o.parity * 0.5f) * (innerRadius * 2f);
-            var y = -o.row * (InnerRadiusToOuterRadius(innerRadius) * 1.5f);
-
-            return new float2(x, y);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float3 OffsetToPositionW(OffsetHexCoordinates o, float innerRadius, float4x4 localToWorldMatrix)
-        {
-            var offsetToPositionL = OffsetToPositionL(o, innerRadius);
-
-            return math.mul(localToWorldMatrix, new float4(offsetToPositionL.x, 0f, offsetToPositionL.y, 1f)).xyz;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static OffsetHexCoordinates PositionLToOffset(float2 v, float innerRadius)
-        {
-            var row = Mathf.RoundToInt(-v.y / (InnerRadiusToOuterRadius(innerRadius) * 1.5f));
-            var column = Mathf.RoundToInt((v.x / (innerRadius * 2f)) - ((row & 1) * 0.5f));
-
-            return new OffsetHexCoordinates(column, row);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static OffsetHexCoordinates PositionWToOffset(float3 v, float innerRadius, float4x4 worldToLocalMatrix)
-        {
-            var positionL = math.mul(worldToLocalMatrix, new float4(v, 1f));
-
-            return PositionLToOffset(positionL.xz, innerRadius);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static (float x, float y, float z) OffsetWToPositionW(int column, int height, int row, float innerRadius)
         {
             var heightParity = (math.abs(height * 3) + height) % 3;
@@ -72,21 +26,24 @@ namespace UnityEngine
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static (int column, int height, int row) PositionWToOffsetW(float x, float y, float z, float innerRadius)
         {
-            var height = (int) math.round((y / innerRadius) / 1.632993162f);
+            var heightF = (y / innerRadius) / 1.632993162f;
+            var height = math.select((int) heightF, (int) (heightF + 1f), math.frac(heightF) >= 0.5f);
             var heightParity = (math.abs(height * 3) + height) % 3;
 
-            var row = (int) math.round(((z / innerRadius) - (heightParity * 0.577350269f)) / 1.732050807f);
+            var rowF = ((z / innerRadius) - (heightParity * 0.577350269f)) / 1.732050807f;
+            var row = math.select((int) rowF, (int) (rowF + 1f), math.frac(rowF) >= 0.5f);
             var rowParity = row & 1;
 
-            var column = (int) math.round(((x / innerRadius) - (heightParity * 1f) - (rowParity * 1f)) / 2);
+            var columnF = ((x / innerRadius) - (heightParity * 1f) - (rowParity * 1f)) / 2;
+            var column = math.select((int) columnF, (int) (columnF + 1f), math.frac(columnF) >= 0.5f);
 
             return (column, height, row);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (int q, int s, int r) OffsetWToAxialW(int column, int height, int row)
+        public static (int q, int t, int r) OffsetWToAxialW(int column, int height, int row)
         {
-            var s = height;
+            var t = height;
             var heightParity = (math.abs(height * 3) + height) % 3;
 
             var r = row - ((height - heightParity) / 3);
@@ -94,13 +51,13 @@ namespace UnityEngine
 
             var q = column - ((height - heightParity) / 3) - ((row - rowParity) / 2);
 
-            return (q, s, r);
+            return (q, t, r);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (int column, int height, int row) AxialWToOffsetW(int q, int s, int r)
+        public static (int column, int height, int row) AxialWToOffsetW(int q, int t, int r)
         {
-            var height = s;
+            var height = t;
             var heightParity = (math.abs(height * 3) + height) % 3;
 
             var row = r + ((height - heightParity) / 3);
@@ -112,9 +69,9 @@ namespace UnityEngine
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int ManhattanLength(int q, int s, int r)
+        public static int ManhattanLength(int q, int t, int r)
         {
-            return (math.abs(q) + math.abs(s) + math.abs(r) + math.abs(q + s + r)) / 2;
+            return (math.abs(q) + math.abs(t) + math.abs(r) + math.abs(q + t + r)) / 2;
         }
     }
 }
