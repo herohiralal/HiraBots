da7df88a762d9a9fc4a14387f1399195550e264d
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-01-07 08:54:24 +0530

    Added more accessible SIMD methods.

--- Scripts/Runtime/Export/TacMap/HexTacMapUtility.cs
+++ Scripts/Runtime/Export/TacMap/HexTacMapUtility.cs
@@ -24,21 +24,45 @@ namespace UnityEngine
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (float4 x, float4 y, float4 z) OffsetWToPositionW(int4 column, int4 height, int4 row, float innerRadius)
+        public static float4x3 IndividualOffsetsWToLinearPositionsW(int4x3 individualOffsets, float innerRadius)
         {
-            var heightParity = (math.abs(height * 3) + height) % 3;
-            var rowParity = row & 1;
+            var columns = individualOffsets.c0;
+            var heights = individualOffsets.c1;
+            var rows = individualOffsets.c2;
 
-            var y = height * new float4(1.6329933162f);
+            var heightParities = (math.abs(heights * 3) + heights) % 3;
+            var rowParities = rows & 1;
 
-            var z = ((row * new float4(1.732050807f))
-                     + (heightParity * new float4(0.577350269f)));
+            var y = heights * new float4(1.6329933162f);
 
-            var x = ((column * new float4(2f))
-                     + (heightParity * new float4(1f))
-                     + (rowParity * new float4(1f)));
+            var z = ((rows * new float4(1.732050807f))
+                     + (heightParities * new float4(0.577350269f)));
 
-            return (x * innerRadius, y * innerRadius, z * innerRadius);
+            var x = ((columns * new float4(2f))
+                     + (heightParities * new float4(1f))
+                     + (rowParities * new float4(1f)));
+
+            return new float4x3(x * innerRadius, y * innerRadius, z * innerRadius);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float4x3 OffsetWToLinearPositionsW(int3x4 offsetCoordinates, float innerRadius)
+        {
+            return IndividualOffsetsWToLinearPositionsW(math.transpose(offsetCoordinates), innerRadius);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3x4 IndividualOffsetsWToPositionW(int4x3 individualOffsets, float innerRadius)
+        {
+            return math.transpose(IndividualOffsetsWToLinearPositionsW(individualOffsets, innerRadius));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3x4 OffsetWToPositionW(int3x4 offsetCoordinates, float innerRadius)
+        {
+            var linearPositions = IndividualOffsetsWToLinearPositionsW(math.transpose(offsetCoordinates), innerRadius);
+
+            return math.transpose(linearPositions);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -59,20 +83,44 @@ namespace UnityEngine
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static (int4 column, int4 height, int4 row) PositionWToOffsetW(float4 x, float4 y, float4 z, float innerRadius)
+        public static int4x3 LinearPositionsWToIndividualOffsetsW(float4x3 linearPositions, float innerRadius)
         {
+            var x = linearPositions.c0;
+            var y = linearPositions.c1;
+            var z = linearPositions.c2;
+
             var heightF = (y / innerRadius) / 1.632993162f;
-            var height = math.select((int4) heightF, (int4) (heightF + 1f), math.frac(heightF) > 0.5f);
-            var heightParity = (math.abs(height * 3) + height) % 3;
+            var heights = math.select((int4) heightF, (int4) (heightF + 1f), math.frac(heightF) > 0.5f);
+            var heightParities = (math.abs(heights * 3) + heights) % 3;
 
-            var rowF = ((z / innerRadius) - (heightParity * new float4(0.577350269f))) / 1.732050807f;
-            var row = math.select((int4) rowF, (int4) (rowF + 1f), math.frac(rowF) > 0.5f);
-            var rowParity = row & 1;
+            var rowF = ((z / innerRadius) - (heightParities * new float4(0.577350269f))) / 1.732050807f;
+            var rows = math.select((int4) rowF, (int4) (rowF + 1f), math.frac(rowF) > 0.5f);
+            var rowParity = rows & 1;
 
-            var columnF = ((x / innerRadius) - (heightParity * new float4(1f)) - (rowParity * new float4(1f))) / 2;
-            var column = math.select((int4) columnF, (int4) (columnF + 1f), math.frac(columnF) > 0.5f);
+            var columnF = ((x / innerRadius) - (heightParities * new float4(1f)) - (rowParity * new float4(1f))) / 2;
+            var columnParities = math.select((int4) columnF, (int4) (columnF + 1f), math.frac(columnF) > 0.5f);
 
-            return (column, height, row);
+            return new int4x3(columnParities, heights, rows);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int4x3 PositionWToIndividualOffsetsW(float3x4 vectorCoordinates, float innerRadius)
+        {
+            return LinearPositionsWToIndividualOffsetsW(math.transpose(vectorCoordinates), innerRadius);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int3x4 LinearPositionsWToOffsetW(float4x3 linearPositions, float innerRadius)
+        {
+            return math.transpose(LinearPositionsWToIndividualOffsetsW(linearPositions, innerRadius));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int3x4 PositionWToOffsetW(float3x4 vectorCoordinates, float innerRadius)
+        {
+            var individualOffsets = LinearPositionsWToIndividualOffsetsW(math.transpose(vectorCoordinates), innerRadius);
+
+            return math.transpose(individualOffsets);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
