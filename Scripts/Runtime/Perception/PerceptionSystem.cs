2877243fbf94daaf80a326807fc88ac1957059c3
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-01-27 16:31:31 +0530

    Implemented: stimuli layers and command buffer execution for stimulus database.

--- Scripts/Runtime/Perception/PerceptionSystem.cs
+++ Scripts/Runtime/Perception/PerceptionSystem.cs
@@ -12,26 +12,36 @@ namespace HiraBots
         private static Dictionary<HiraBotSensor, int> s_SensorsLookUpTable;
         private static int s_SensorsCount;
 
-        private static NativeArray<float4> s_StimuliPositions;
-        private static NativeArray<int> s_StimuliTypes;
-        private static NativeArray<int> s_StimuliAssociatedObjects;
-        private static int s_StimuliCount;
+        private static NativeArray<float4>[] s_StimuliPositions;
+        private static NativeArray<int>[] s_StimuliAssociatedObjects;
+        private static NativeArray<int>[] s_Stimuli;
+        private static Dictionary<int, (byte type, int index)> s_StimuliLookUpTable;
+        private static int[] s_StimuliCounts;
 
-        private static ulong s_Id = 0;
+        private static int s_Id = 0;
 
         internal static void Initialize()
         {
-            s_Id = 0;
+            s_Id = int.MinValue;
 
             s_SensorsStimulusMasks = new NativeArray<int>(8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
             s_Sensors = new HiraBotSensor[8];
             s_SensorsLookUpTable = new Dictionary<HiraBotSensor, int>(8);
             s_SensorsCount = 0;
 
-            s_StimuliPositions = new NativeArray<float4>(8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-            s_StimuliTypes = new NativeArray<int>(8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-            s_StimuliAssociatedObjects = new NativeArray<int>(8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-            s_StimuliCount = 0;
+            s_StimuliPositions = new NativeArray<float4>[32];
+            s_StimuliAssociatedObjects = new NativeArray<int>[32];
+            s_Stimuli = new NativeArray<int>[32];
+            s_StimuliLookUpTable = new Dictionary<int, (byte, int)>();
+            s_StimuliCounts = new int[32];
+
+            for (var i = 0; i < 32; i++)
+            {
+                s_StimuliPositions[i] = new NativeArray<float4>(0, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                s_StimuliAssociatedObjects[i] = new NativeArray<int>(0, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                s_Stimuli[i] = new NativeArray<int>(0, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+                s_StimuliCounts[i] = 0;
+            }
 
             InitializeSensorsDatabaseCommandBuffer();
             InitializeStimuliDatabaseCommandBuffer();
@@ -45,17 +55,26 @@ namespace HiraBots
             ShutdownStimuliDatabaseCommandBuffer();
             ShutdownSensorsDatabaseCommandBuffer();
 
-            s_StimuliCount = 0;
-            s_StimuliAssociatedObjects.Dispose();
-            s_StimuliTypes.Dispose();
-            s_StimuliPositions.Dispose();
+            for (var i = 0; i < 32; i++)
+            {
+                s_StimuliCounts[i] = 0;
+                s_Stimuli[i].Dispose();
+                s_StimuliAssociatedObjects[i].Dispose();
+                s_StimuliPositions[i].Dispose();
+            }
+
+            s_StimuliCounts = null;
+            s_StimuliLookUpTable = null;
+            s_Stimuli = null;
+            s_StimuliAssociatedObjects = null;
+            s_StimuliPositions = null;
 
             s_SensorsCount = 0;
             s_SensorsLookUpTable.Clear();
             s_Sensors = new HiraBotSensor[0];
             s_SensorsStimulusMasks.Dispose();
 
-            s_Id = 0;
+            s_Id = int.MinValue;
         }
 
         internal static void UpdateDatabase()
@@ -66,7 +85,13 @@ namespace HiraBots
 
         internal static void Tick(float deltaTime)
         {
-            if (s_SensorsCount == 0 && s_StimuliCount == 0)
+            // if the time is frozen, ignore perception system stuff
+            if (deltaTime == 0f)
+            {
+                return;
+            }
+
+            if (s_SensorsCount == 0 && s_StimuliLookUpTable.Count == 0)
             {
                 return;
             }
--- Scripts/Runtime/Perception/_StimuliDatabaseCommandBuffer.cs
+++ Scripts/Runtime/Perception/_StimuliDatabaseCommandBuffer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,63 +10,63 @@ namespace HiraBots
     {
         private struct AddStimulusCommand
         {
-            internal AddStimulusCommand(ulong id, float3 position, int stimulusType, int associatedObject)
+            internal AddStimulusCommand(byte stimulusType, float3 position, int associatedObject, int id)
             {
-                this.id = id;
-                this.position = position;
                 this.stimulusType = stimulusType;
+                this.position = position;
                 this.associatedObject = associatedObject;
+                this.id = id;
             }
 
-            internal ulong id { get; }
+            internal byte stimulusType { get; }
             internal float3 position { get; }
-            internal int stimulusType { get; }
             internal int associatedObject { get; }
+            internal int id { get; }
         }
 
         private struct RemoveStimulusCommand
         {
-            internal RemoveStimulusCommand(ulong id)
+            internal RemoveStimulusCommand(int id)
             {
                 this.id = id;
             }
 
-            internal ulong id { get; }
+            internal int id { get; }
         }
 
         private struct ChangeStimulusPositionCommand
         {
-            internal ChangeStimulusPositionCommand(ulong id, float3 newPos)
+            internal ChangeStimulusPositionCommand(int id, float3 newPos)
             {
                 this.id = id;
                 this.newPos = newPos;
             }
 
-            internal ulong id { get; }
+            internal int id { get; }
             internal float3 newPos { get; }
         }
 
         private struct ChangeStimulusTypeCommand
         {
-            internal ChangeStimulusTypeCommand(ulong id, int newStimulusType)
+            internal ChangeStimulusTypeCommand(int id, byte newStimulusType)
             {
                 this.id = id;
                 this.newStimulusType = newStimulusType;
             }
 
-            internal ulong id { get; }
-            internal int newStimulusType { get; }
+            internal int id { get; }
+            internal byte newStimulusType { get; }
         }
 
         private struct ChangeStimulusAssociatedObjectCommand
         {
-            internal ChangeStimulusAssociatedObjectCommand(ulong id, int newAssociatedObject)
+            internal ChangeStimulusAssociatedObjectCommand(int id, int newAssociatedObject)
             {
                 this.id = id;
                 this.newAssociatedObject = newAssociatedObject;
             }
 
-            internal ulong id { get; }
+            internal int id { get; }
             internal int newAssociatedObject { get; }
         }
 
@@ -108,35 +109,35 @@ namespace HiraBots
             s_StimuliDatabaseCommandTypes = null;
         }
 
-        internal static ulong AddStimulus(Vector3 position, int stimulusType, int associatedObject)
+        internal static int AddStimulus(byte stimulusType, Vector3 position, int associatedObject)
         {
             s_Id++;
 
             s_StimuliDatabaseCommandTypes.Enqueue(StimuliDatabaseCommandType.AddStimulus);
-            s_AddStimulusCommands.Enqueue(new AddStimulusCommand(s_Id, position, stimulusType, associatedObject));
+            s_AddStimulusCommands.Enqueue(new AddStimulusCommand(stimulusType, position, associatedObject, s_Id));
 
             return s_Id;
         }
 
-        internal static void RemoveStimulus(ulong id)
+        internal static void RemoveStimulus(int id)
         {
             s_StimuliDatabaseCommandTypes.Enqueue(StimuliDatabaseCommandType.RemoveStimulus);
             s_RemoveStimulusCommands.Enqueue(new RemoveStimulusCommand(id));
         }
 
-        internal static void ChangeStimulusPosition(ulong id, Vector3 newPos)
+        internal static void ChangeStimulusPosition(int id, Vector3 newPos)
         {
             s_StimuliDatabaseCommandTypes.Enqueue(StimuliDatabaseCommandType.ChangeStimulusPosition);
             s_ChangeStimulusPositionCommands.Enqueue(new ChangeStimulusPositionCommand(id, newPos));
         }
 
-        internal static void ChangeStimulusType(ulong id, int newType)
+        internal static void ChangeStimulusType(int id, byte newType)
         {
             s_StimuliDatabaseCommandTypes.Enqueue(StimuliDatabaseCommandType.ChangeStimulusType);
             s_ChangeStimulusTypeCommands.Enqueue(new ChangeStimulusTypeCommand(id, newType));
         }
 
-        internal static void ChangeStimulusAssociatedObject(ulong id, int newAssociatedObject)
+        internal static void ChangeStimulusAssociatedObject(int id, int newAssociatedObject)
         {
             s_StimuliDatabaseCommandTypes.Enqueue(StimuliDatabaseCommandType.ChangeStimulusAssociatedObject);
             s_ChangeStimulusAssociatedObjectCommands.Enqueue(new ChangeStimulusAssociatedObjectCommand(id, newAssociatedObject));
@@ -150,22 +151,64 @@ namespace HiraBots
                 {
                     case StimuliDatabaseCommandType.AddStimulus:
                     {
+                        var cmd = s_AddStimulusCommands.Dequeue();
+                        if (s_StimuliLookUpTable.ContainsKey(cmd.id))
+                        {
+                            break;
+                        }
+
+                        AddStimulus(cmd.stimulusType, new float4(cmd.position, 1), cmd.associatedObject, cmd.id);
                         break;
                     }
                     case StimuliDatabaseCommandType.RemoveStimulus:
                     {
+                        var cmd = s_RemoveStimulusCommands.Dequeue();
+                        if (!s_StimuliLookUpTable.TryGetValue(cmd.id, out var toRemove))
+                        {
+                            break;
+                        }
+
+                        RemoveStimulusAt(toRemove.type, toRemove.index);
                         break;
                     }
                     case StimuliDatabaseCommandType.ChangeStimulusPosition:
                     {
+                        var cmd = s_ChangeStimulusPositionCommands.Dequeue();
+                        if (!s_StimuliLookUpTable.TryGetValue(cmd.id, out var i))
+                        {
+                            break;
+                        }
+
+                        s_StimuliPositions[i.type][i.index] = new float4(cmd.newPos, 1);
                         break;
                     }
                     case StimuliDatabaseCommandType.ChangeStimulusType:
                     {
+                        var cmd = s_ChangeStimulusTypeCommands.Dequeue();
+                        if (!s_StimuliLookUpTable.TryGetValue(cmd.id, out var i))
+                        {
+                            break;
+                        }
+
+                        // don't inline these, they're gonna get overwritten when removing previous
+                        var pos = s_StimuliPositions[i.type][i.index];
+                        var asObj = s_StimuliAssociatedObjects[i.type][i.index];
+                        var id = s_Stimuli[i.type][i.index];
+
+                        RemoveStimulusAt(i.type, i.index);
+                        AddStimulus(cmd.newStimulusType, pos, asObj, id);
+
                         break;
                     }
                     case StimuliDatabaseCommandType.ChangeStimulusAssociatedObject:
                     {
+                        var cmd = s_ChangeStimulusAssociatedObjectCommands.Dequeue();
+                        if (!s_StimuliLookUpTable.TryGetValue(cmd.id, out var i))
+                        {
+                            break;
+                        }
+
+                        s_StimuliAssociatedObjects[i.type][i.index] = cmd.newAssociatedObject;
                         break;
                     }
                     default:
@@ -187,6 +230,51 @@ namespace HiraBots
             Debug.Break();
             Debug.LogError("Command buffers not correctly cleared in Perception System stimuli database.");
 #endif
+        }
+
+        private static void AddStimulus(byte stimulusType, float4 position, int associatedObject, int id)
+        {
+            var currentStimuliCount = s_StimuliCounts[stimulusType];
+
+            if (currentStimuliCount == s_Stimuli[stimulusType].Length)
+            {
+                // reallocation time
+                s_StimuliPositions[stimulusType]
+                    .Reallocate(currentStimuliCount + 8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+
+                s_StimuliAssociatedObjects[stimulusType]
+                    .Reallocate(currentStimuliCount + 8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+
+                s_Stimuli[stimulusType]
+                    .Reallocate(currentStimuliCount + 8, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+            }
+
+            s_StimuliPositions[stimulusType][currentStimuliCount] = position;
+
+            s_StimuliAssociatedObjects[stimulusType][currentStimuliCount] = associatedObject;
+
+            s_StimuliLookUpTable.Add(id, (stimulusType, currentStimuliCount));
+
+            s_Stimuli[stimulusType][currentStimuliCount] = id;
+
+            s_StimuliCounts[stimulusType]++;
+        }
+
+        private static void RemoveStimulusAt(byte type, int index)
+        {
+            var lastStimulusIndex = s_StimuliCounts[type] - 1;
+            var lastStimulus = s_Stimuli[type][lastStimulusIndex];
+
+            s_StimuliPositions[type][index] = s_StimuliPositions[type][lastStimulusIndex];
+
+            s_StimuliAssociatedObjects[type][index] = s_StimuliAssociatedObjects[type][lastStimulusIndex];
+
+            s_StimuliLookUpTable[lastStimulus] = (type, index);
+            s_StimuliLookUpTable.Remove(s_Stimuli[type][index]);
+
+            s_Stimuli[type][index] = lastStimulus;
+
+            s_StimuliCounts[type]--;
         }
     }
 }
