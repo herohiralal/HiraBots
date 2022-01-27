ddfa3775566051021ec6e9cf12e3ec4c81ddb67b
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-01-27 08:10:37 +0530

    Created skeleton for perception system.

---
+++ Scripts/Runtime/Export/Perception/HiraBotsDefaultStimulusMask.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityEngine.AI
+{
+    [System.Flags]
+    public enum HiraBotsDefaultStimulusMask : int
+    {
+        None = 0,
+        Sight = 1 << 0,
+        Sound = 1 << 1,
+    }
+}
---
+++ Scripts/Runtime/Perception/PerceptionSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
+
+namespace HiraBots
+{
+    internal static class PerceptionSystem
+    {
+        private static SensorComponent[] s_Sensors;
+        private static Dictionary<SensorComponent, int> s_SensorsLookUpTable;
+        private static int s_SensorsCount;
+
+        private static NativeArray<float3> s_StimuliPositions;
+        private static NativeArray<int> s_AssociatedGameObjects;
+        private static int s_StimuliCount;
+
+        private static ulong s_Id = 0;
+
+        internal static void Initialize()
+        {
+            s_Id = 0;
+        }
+
+        internal static void Shutdown()
+        {
+            s_Id = 0;
+        }
+
+        internal static unsafe void Tick(float deltaTime)
+        {
+            // for all the stimuli
+            // for all the sensors
+            // check if the masks match and schedule a bounds check job
+            // can probably run main module ticking job here
+
+            // synchronize and make sure all bounds check jobs are done
+            // once the bounds have been figured out, start LOS check and nav-distance check jobs
+            // for the ones that require them
+            // for the ones that don't, update their 'perceived game objects' sets
+
+            // complete all LOS (or nav-distance but not both) check jobs and update their
+            // perceived game object sets
+
+            // do the same with the other one
+        }
+    }
+}
