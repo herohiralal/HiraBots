597b412c0dc613364b480f4dc56a419057ab239e
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-02 13:48:51 +0530

    Finished NativeList.

--- Scripts/Runtime/Utils/Collections/NativeList.cs
+++ Scripts/Runtime/Utils/Collections/NativeList.cs
@@ -19,7 +19,7 @@ namespace HiraBots
     [NativeContainerSupportsDeallocateOnJobCompletion]
     [DebuggerDisplay("Count = {count}, Capacity = {capacity}")]
     [DebuggerTypeProxy(typeof(NativeListDebugView<>))]
-    internal unsafe struct NativeList<T> : IDisposable where T : struct
+    internal unsafe struct NativeList<T> : IDisposable, IEquatable<NativeList<T>> where T : struct
     {
         [NativeDisableUnsafePtrRestriction] private NativeListMetadata* m_Metadata;
 
@@ -30,22 +30,27 @@ namespace HiraBots
 
         private Allocator m_AllocatorLabel;
 
+        internal NativeList(Allocator allocator)
+        {
+            Allocate(8, allocator, out this);
+        }
+
         internal NativeList(int capacity, Allocator allocator)
         {
-            Allocate(capacity, allocator, out this);
+            Allocate((capacity + 7) & ~7, allocator, out this);
         }
 
         internal NativeList(NativeArray<T> array, Allocator allocator)
         {
-            var length = array.Length;
+            var length = (array.Length + 7) & ~7;
 
             Allocate(length, allocator, out this);
 
             var src = array.GetUnsafeReadOnlyPtr();
             var dst = m_Metadata->m_Buffer;
 
-            UnsafeUtility.MemCpy(src, dst, UnsafeUtility.SizeOf<T>() * length);
-            m_Metadata->m_Count = length;
+            UnsafeUtility.MemCpy(dst, src, UnsafeUtility.SizeOf<T>() * array.Length);
+            m_Metadata->m_Count = array.Length;
         }
 
         private static void Allocate(int capacity, Allocator allocator, out NativeList<T> list)
@@ -83,13 +88,34 @@ namespace HiraBots
 #endif
         }
 
+        private void EnsureCapacityToInsert(int countToInsert)
+        {
+            var newCapacity = m_Metadata->m_Count + countToInsert;
+
+            if (m_Metadata->m_Capacity >= newCapacity)
+            {
+                return;
+            }
+
+            var cap = (newCapacity + 7) & ~7;
+
+            var size = (long) UnsafeUtility.SizeOf<T>() * cap;
+
+            var newBuffer = UnsafeUtility.Malloc(size, UnsafeUtility.AlignOf<T>(), m_AllocatorLabel);
+            UnsafeUtility.MemCpy(newBuffer, m_Metadata->m_Buffer, m_Metadata->m_Count * UnsafeUtility.SizeOf<T>());
+
+            UnsafeUtility.Free(m_Metadata->m_Buffer, m_AllocatorLabel);
+            m_Metadata->m_Buffer = newBuffer;
+            m_Metadata->m_Capacity = cap;
+        }
+
         private void Deallocate()
         {
             UnsafeUtility.Free(m_Metadata->m_Buffer, m_AllocatorLabel);
             m_Metadata->m_Buffer = null;
-
             m_Metadata->m_Capacity = 0;
             m_Metadata->m_Count = 0;
+
             UnsafeUtility.Free(m_Metadata, m_AllocatorLabel);
             m_Metadata = null;
 
@@ -132,6 +158,240 @@ namespace HiraBots
         {
             public NativeList<T> m_Container;
             public void Execute() => m_Container.Deallocate();
+        }
+
+        internal int count
+        {
+            get
+            {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+                AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
+
+                if (m_Metadata == null)
+                {
+                    throw new InvalidOperationException("NativeList has not been created.");
+                }
+#endif
+                return m_Metadata->m_Count;
+            }
+        }
+
+        internal int capacity
+        {
+            get
+            {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+                AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
+
+                if (m_Metadata == null)
+                {
+                    throw new InvalidOperationException("NativeList has not been created.");
+                }
+#endif
+                return m_Metadata->m_Capacity;
+            }
+        }
+
+        internal bool isCreated => m_Metadata != null;
+
+        internal T this[int index]
+        {
+            get
+            {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+                AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
+
+                if (m_Metadata == null)
+                {
+                    throw new InvalidOperationException("NativeList has not been created.");
+                }
+
+                if (index < 0 || index >= m_Metadata->m_Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Index {index} is out of range [0, {m_Metadata->m_Count}).");
+                }
+#endif
+                return UnsafeUtility.ReadArrayElement<T>(m_Metadata->m_Buffer, index);
+            }
+            [WriteAccessRequired] set
+            {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+                AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
+
+                if (m_Metadata == null)
+                {
+                    throw new InvalidOperationException("NativeList has not been created.");
+                }
+
+                if (index < 0 || index >= m_Metadata->m_Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Index {index} is out of range [0, {m_Metadata->m_Count}).");
+                }
+#endif
+                UnsafeUtility.WriteArrayElement<T>(m_Metadata->m_Buffer, index, value);
+            }
+        }
+
+        internal int FirstIndexOf<TEquatable>(TEquatable item) where TEquatable : struct, IEquatable<T>
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
+
+            if (m_Metadata == null)
+            {
+                throw new InvalidOperationException("NativeList has not been created.");
+            }
+#endif
+            var c = m_Metadata->m_Count;
+            for (var i = 0; i < c; i++)
+            {
+                var element = UnsafeUtility.ReadArrayElement<T>(m_Metadata->m_Buffer, i);
+                if (item.Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        internal int LastIndexOf<TEquatable>(TEquatable item) where TEquatable : struct, IEquatable<T>
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckReadAndThrow(m_Safety);
+
+            if (m_Metadata == null)
+            {
+                throw new InvalidOperationException("NativeList has not been created.");
+            }
+#endif
+            for (var i = m_Metadata->m_Count - 1; i >= 0; i--)
+            {
+                var element = UnsafeUtility.ReadArrayElement<T>(m_Metadata->m_Buffer, i);
+                if (item.Equals(element))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        internal void Add(T item)
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
+
+            if (m_Metadata == null)
+            {
+                throw new InvalidOperationException("NativeList has not been created.");
+            }
+#endif
+
+            EnsureCapacityToInsert(1);
+
+            UnsafeUtility.WriteArrayElement<T>(m_Metadata->m_Buffer, m_Metadata->m_Count, item);
+            m_Metadata->m_Count++;
+        }
+
+        internal void RemoveFirst<TEquatable>(TEquatable item) where TEquatable : struct, IEquatable<T>
+        {
+            var index = FirstIndexOf(item);
+
+            if (index != -1)
+            {
+                RemoveAt(index);
+            }
+        }
+
+        internal void RemoveLast<TEquatable>(TEquatable item) where TEquatable : struct, IEquatable<T>
+        {
+            var index = LastIndexOf(item);
+
+            if (index != -1)
+            {
+                RemoveAt(index);
+            }
+        }
+
+        internal void InsertAt(int index, T item)
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
+
+            if (m_Metadata == null)
+            {
+                throw new InvalidOperationException("NativeList has not been created.");
+            }
+
+            if (index < 0 || index >= m_Metadata->m_Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Index {index} is out of range [0, {m_Metadata->m_Count}).");
+            }
+#endif
+
+            EnsureCapacityToInsert(1);
+
+            var copySize = m_Metadata->m_Count - index;
+
+            var src = (void*) ((byte*) m_Metadata->m_Buffer + (index * UnsafeUtility.SizeOf<T>()));
+            var dst = (void*) ((byte*) m_Metadata->m_Buffer + ((index + 1) * UnsafeUtility.SizeOf<T>()));
+
+            UnsafeUtility.MemMove(dst, src, copySize * UnsafeUtility.SizeOf<T>());
+
+            UnsafeUtility.WriteArrayElement(m_Metadata->m_Buffer, index, item);
+            m_Metadata->m_Count++;
+        }
+
+        internal void RemoveAt(int index)
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            AtomicSafetyHandle.CheckWriteAndThrow(m_Safety);
+
+            if (m_Metadata == null)
+            {
+                throw new InvalidOperationException("NativeList has not been created.");
+            }
+
+            if (index < 0 || index >= m_Metadata->m_Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Index {index} is out of range [0, {m_Metadata->m_Count}).");
+            }
+#endif
+
+            var copySize = m_Metadata->m_Count - index - 1;
+
+            var dst = (void*) ((byte*) m_Metadata->m_Buffer + (index * UnsafeUtility.SizeOf<T>()));
+            var src = (void*) ((byte*) m_Metadata->m_Buffer + ((index + 1) * UnsafeUtility.SizeOf<T>()));
+
+            UnsafeUtility.MemMove(dst, src, copySize * UnsafeUtility.SizeOf<T>());
+
+            m_Metadata->m_Count--;
+        }
+
+        public bool Equals(NativeList<T> other)
+        {
+            return m_Metadata == other.m_Metadata;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NativeList<T> other && Equals(other);
+        }
+
+        public static bool operator ==(NativeList<T> left, NativeList<T> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(NativeList<T> left, NativeList<T> right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override int GetHashCode()
+        {
+            return unchecked((int) (long) m_Metadata);
         }
     }
 
