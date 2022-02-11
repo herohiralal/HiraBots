6ad1aac82f10922327625c835b74bf82bcace06e
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-11 05:32:52 +0530

    Created NavigationTypes.

---
+++ Scripts/Editor/GUI/PropertyDrawers/NavAreaMaskPropertyDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEditor.AI;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace HiraBots.Editor
+{
+    [CustomPropertyDrawer(typeof(NavAreaMask))]
+    internal class NavAreaMaskPropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 21f;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var valueProperty = property.FindPropertyRelative("m_Value");
+            if (valueProperty == null)
+            {
+                EditorGUI.HelpBox(position, "Could not find value property.", MessageType.Error);
+                return;
+            }
+
+            position.height = 19f;
+            position.width -= 21f;
+
+            valueProperty.intValue = EditorGUI.MaskField(position, label, valueProperty.intValue,
+                GameObjectUtility.GetNavMeshAreaNames());
+
+            position.x += position.width + 2f;
+            position.width = 19f;
+
+            var settingsIcon = EditorGUIUtility.IconContent(EditorGUIUtility.isProSkin ? "d_SettingsIcon" : "SettingsIcon");
+            var buttonContent = new GUIContent(settingsIcon);
+
+            if (GUI.Button(position, buttonContent))
+            {
+                NavMeshEditorHelpers.OpenAreaSettings();
+            }
+        }
+    }
+}
---
+++ Scripts/Runtime/Export/Misc/NavigationTypes.cs
@@ -0,0 +1,34 @@
+﻿namespace UnityEngine.AI
+{
+    [System.Serializable]
+    public struct NavAgentType
+    {
+        [SerializeField] private int m_Value;
+
+        public static implicit operator int(NavAgentType x)
+        {
+            return x.m_Value;
+        }
+
+        public static implicit operator NavAgentType(int value)
+        {
+            return new NavAgentType { m_Value = value };
+        }
+    }
+
+    [System.Serializable]
+    public struct NavAreaMask
+    {
+        [SerializeField] private int m_Value;
+
+        public static implicit operator int(NavAreaMask x)
+        {
+            return x.m_Value;
+        }
+
+        public static implicit operator NavAreaMask(int value)
+        {
+            return new NavAreaMask { m_Value = value };
+        }
+    }
+}
