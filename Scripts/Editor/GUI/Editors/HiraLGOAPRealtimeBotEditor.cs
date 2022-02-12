74c689cd50bb39c9b134a098cf85fb2db778752d
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-12 13:51:47 +0530

    Added: looping display to realtime bot.

--- Scripts/Editor/GUI/Editors/HiraLGOAPRealtimeBotEditor.cs
+++ Scripts/Editor/GUI/Editors/HiraLGOAPRealtimeBotEditor.cs
@@ -269,6 +269,15 @@ namespace HiraBots.Editor
 
                     var containerName = domain.GetContainerName(i, currentContainerIndex);
 
+                    var looping = i != 0
+                                  && !domain.IsTaskAbstract(i, currentContainerIndex)
+                                  && domain.LoopTask(i, currentContainerIndex);
+
+                    if (looping)
+                    {
+                        containerName += " (Looping)";
+                    }
+
                     var progressBarRect = EditorGUI.PrefixLabel(EditorGUILayout.GetControlRect(), GUIHelpers.TempContent(containerName));
 
                     float progress;
