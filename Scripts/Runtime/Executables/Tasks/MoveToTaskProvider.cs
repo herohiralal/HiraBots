881940afb17cc6ca2a92317f17ef8954407b8a11
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-13 11:24:01 +0530

    Created MoveToTrackedLocationTask.

--- Scripts/Runtime/Executables/Tasks/MoveToTaskProvider.cs
+++ Scripts/Runtime/Executables/Tasks/MoveToTaskProvider.cs
@@ -8,6 +8,9 @@ namespace HiraBots
         [Tooltip("Target location.")]
         [SerializeField] private UnityEngine.AI.BlackboardTemplate.KeySelector m_Target;
 
+        [Tooltip("Track moving target.")]
+        [SerializeField] private bool m_TrackMovingTarget = false;
+
         [Tooltip("Tolerance for reaching the destination.")]
         [SerializeField] private float m_Tolerance = 0.25f;
 
@@ -18,11 +21,18 @@ namespace HiraBots
         {
             var key = m_Target.selectedKey;
 
+            if (!(archetype is IHiraBotArchetype<NavMeshAgent> navigatingAgent))
+            {
+                return ErrorExecutable.Get($"{archetype.gameObject.name} is not a navigating agent.");
+            }
+
             switch (key.keyType)
             {
-                case UnityEngine.AI.BlackboardKeyType.Vector when archetype is IHiraBotArchetype<NavMeshAgent> navigatingAgent:
+                case UnityEngine.AI.BlackboardKeyType.Vector when !m_TrackMovingTarget:
                     var location = blackboard.GetVectorValue(key.name);
                     return MoveToLocationTask.Get(navigatingAgent.component, location, m_Tolerance, m_Speed);
+                case UnityEngine.AI.BlackboardKeyType.Vector when m_TrackMovingTarget:
+                    return MoveToTrackedLocationTask.Get(blackboard, navigatingAgent.component, m_Target.selectedKey.name, m_Tolerance, m_Speed);
                 default:
                     return ErrorExecutable.Get($"Can not execute {name}.");
             }
---
+++ Scripts/Runtime/Executables/Tasks/MoveToTrackedLocationTask.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace HiraBots
+{
+    internal class MoveToTrackedLocationTask : IHiraBotsTask
+    {
+        internal static MoveToTrackedLocationTask Get(UnityEngine.AI.BlackboardComponent blackboard,
+            NavMeshAgent agent, string locationKey, float tolerance, float speed)
+        {
+            var output = s_Executables.Count == 0 ? new MoveToTrackedLocationTask() : s_Executables.Pop();
+
+            output.m_Failed = false;
+            output.m_Blackboard = blackboard;
+            output.m_Agent = agent;
+            output.m_LocationKey = locationKey;
+            output.m_Tolerance = tolerance;
+            output.m_Speed = speed;
+
+            return output;
+        }
+
+        private MoveToTrackedLocationTask()
+        {
+        }
+
+        private static readonly Stack<MoveToTrackedLocationTask> s_Executables = new Stack<MoveToTrackedLocationTask>();
+
+        private UnityEngine.AI.BlackboardComponent m_Blackboard;
+        private NavMeshAgent m_Agent;
+        private string m_LocationKey;
+        private Vector3 m_CurrentTarget;
+        private float m_Tolerance;
+        private float m_Speed;
+        private bool m_Failed;
+
+        public void Begin()
+        {
+            m_CurrentTarget = m_Blackboard.GetVectorValue(m_LocationKey);
+
+            if (!m_Agent.SetDestination(m_CurrentTarget))
+            {
+                m_Failed = true;
+                return;
+            }
+
+            m_Agent.speed = m_Speed;
+            m_Agent.stoppingDistance = m_Tolerance / 4;
+            m_Agent.isStopped = false;
+        }
+
+        public HiraBotsTaskResult Execute(float deltaTime)
+        {
+            if (m_Failed)
+            {
+                return HiraBotsTaskResult.Failed;
+            }
+
+            Vector3 currentTarget = m_Blackboard.GetVectorValue(m_LocationKey);
+            if (currentTarget != m_CurrentTarget)
+            {
+                m_CurrentTarget = currentTarget;
+                if (!m_Agent.SetDestination(m_CurrentTarget))
+                {
+                    return HiraBotsTaskResult.Failed;
+                }
+            }
+
+            if (m_Agent.remainingDistance > m_Tolerance / 4)
+            {
+                return HiraBotsTaskResult.InProgress;
+            }
+
+            return HiraBotsTaskResult.Succeeded;
+        }
+
+        public void Abort()
+        {
+            if (m_Agent.isActiveAndEnabled && m_Agent.isOnNavMesh)
+            {
+                m_Agent.isStopped = true;
+            }
+
+            m_Blackboard = default;
+            m_Agent = null;
+            s_Executables.Push(this);
+        }
+
+        public void End(bool success)
+        {
+            if (m_Agent.isActiveAndEnabled && m_Agent.isOnNavMesh)
+            {
+                m_Agent.isStopped = true;
+            }
+
+            m_Blackboard = default;
+            m_Agent = null;
+            s_Executables.Push(this);
+        }
+    }
+}
