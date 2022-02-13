a16e2ed37592b518de1fad9f0654056fbb279ddd
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-02-13 19:05:37 +0530

    Created FailTask.

---
+++ Scripts/Runtime/Executables/Tasks/FailTask.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine.AI;
+
+namespace HiraBots
+{
+    internal class FailTask : IHiraBotsTask
+    {
+        internal static FailTask Get()
+        {
+            return s_Executables.Count == 0 ? new FailTask() : s_Executables.Pop();
+        }
+
+        private FailTask()
+        {
+        }
+
+        private static readonly Stack<FailTask> s_Executables = new Stack<FailTask>();
+
+        public void Begin()
+        {
+        }
+
+        public HiraBotsTaskResult Execute(float deltaTime)
+        {
+            return HiraBotsTaskResult.Failed;
+        }
+
+        public void Abort()
+        {
+            s_Executables.Push(this);
+        }
+
+        public void End(bool success)
+        {
+            s_Executables.Push(this);
+        }
+    }
+}
---
+++ Scripts/Runtime/Executables/Tasks/FailTaskProvider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.AI;
+
+namespace HiraBots
+{
+    internal class FailTaskProvider : HiraBotsTaskProvider
+    {
+        protected override IHiraBotsTask GetTask(UnityEngine.AI.BlackboardComponent blackboard, IHiraBotArchetype archetype)
+        {
+            return FailTask.Get();
+        }
+    }
+}
