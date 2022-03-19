543b1e2072c1446ae4701710085cf81ab24332e2
Rohan Jadav <ro1.darkstar7@gmail.com>
2022-03-20 00:24:55 +0530

    Added optional decorator checking before execution.

--- Scripts/Runtime/Export/BlackboardFunctions/HiraBotsDecoratorBlackboardFunction.cs
+++ Scripts/Runtime/Export/BlackboardFunctions/HiraBotsDecoratorBlackboardFunction.cs
@@ -5,10 +5,24 @@
         [System.Runtime.InteropServices.UnmanagedFunctionPointer(System.Runtime.InteropServices.CallingConvention.Cdecl)]
         public unsafe delegate bool Delegate(in BlackboardComponent.LowLevel blackboard, byte* memory);
 
+        [Tooltip("Whether to automatically test the decorator on the blackboard component prior to execution.")]
+        [SerializeField] private bool m_CheckBeforeExecution = true;
+
+        public bool checkBeforeExecution
+        {
+            get => m_CheckBeforeExecution;
+            set => m_CheckBeforeExecution = value;
+        }
+
         #region Execution
 
         public bool Execute(BlackboardComponent blackboard, bool expected)
         {
+            if (!m_CheckBeforeExecution)
+            {
+                return true;
+            }
+
             try
             {
                 return ExecuteFunction(blackboard, expected);
